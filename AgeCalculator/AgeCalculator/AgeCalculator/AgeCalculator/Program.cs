e5bb634d0a31b615b0d85c3c56e98fe535b3d0ff
ABracken <adriannbracken214@gmail.com>
2015-09-22 19:03:19 -0700

    First commit

---
+++ AgeCalculator/AgeCalculator/AgeCalculator/AgeCalculator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeCalculator
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("What is your birthday? (Enter date in format MM/DD/YYYY):");
+
+            string input = Console.ReadLine();
+
+            DateTime birthDate = DateTime.Parse(input);
+
+            DateTime now = DateTime.Now;
+
+
+
+            TimeSpan Span = now - birthDate;
+
+
+            DateTime Age = DateTime.MinValue + Span;
+
+
+
+            int Years = Age.Year - 1;
+            int Months = Age.Month - 1;
+            int Days = Age.Day - 1;
+            Console.WriteLine("You are " + Years.ToString() + " Years, " + Months.ToString() + " Months, " + Days.ToString() + " Days");
+
+
+
+        }
+    }
---
+++ AgeCalculator/AgeCalculator/AgeCalculator/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AgeCalculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
